c3de7b517d1a2e694385a4bf7114b2d140bd136f
ivalchanka <i.valchenk@hotmail.com>
2023-02-19 10:15:09 +0400

    Added project skeleton.

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset = new Vector3(0, 4.6f, -10.3f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Offset the camera behind the player.
+        transform.position = this.player.transform.position + this.offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private readonly float speed = 20.0f;
+    private readonly float turnSpeed = 25;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.horizontalInput = Input.GetAxis("Horizontal");
+        this.forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * this.speed * this.forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * /*this.turnSpeed **/ this.horizontalInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * this.turnSpeed * this.horizontalInput);
+    }
+}
